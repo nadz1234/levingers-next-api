b3b414eddcc8cbfa0d6162c6dfa1f0a6dbaf18d2
nadz1234 <nadhiren.naidoo@gmail.com>
2022-11-02 22:54:42 +0200

    merged

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,41 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const { response } = require('express');
+const Connection = require('mysql/lib/Connection');
+
+const pool = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'sql11.freesqldatabase.com',
+  user            : 'sql11525614',
+  password        : '9xKG7CAuX5',
+  database        : 'sql11525614'
+
+})
+
+
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+
+  pool.getConnection((err,connection) => {
+
+    if(err) throw err
+    console.log('connected as id')-
+
+    connection.query('SELECT * FROM orders',(err,rows)=> {
+       connection.release()
+
+       if(!err)
+       {
+           res.send(rows)
+       }
+       else
+       {
+           console.log(err)
+       }
+    })
+})
+
+  //res.status(200).json({ name: 'John Doe' })
 }
