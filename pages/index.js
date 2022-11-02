d56d02a61392d03376939d4157bd942fd3007eb1
nadz1234 <nadhiren.naidoo@gmail.com>
2022-11-02 13:59:27 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to nad
+          Welcome to nads
         </h1>
 
         <p className={styles.description}>
