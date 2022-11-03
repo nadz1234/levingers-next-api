0ffb561b8750f158c4a2f6f8fd0c08bbbcaec4e7
nadz1234 <nadhiren.naidoo@gmail.com>
2022-11-03 22:52:37 +0200

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -21,9 +21,9 @@ export default function handler(req, res) {
   pool.getConnection((err,connection) => {
 
     if(err) throw err
-    console.log('connected as id')-
+    console.log('connected as id')
 
-    connection.query('SELECT * FROM orders',(err,rows)=> {
+    connection.query('SELECT * FROM orders WHERE price = ?',[req.params.price],(err,rows)=> {
        connection.release()
 
        if(!err)
