84f7d89c334b29b2a5c4070973c50be0f392d8c0
nadz1234 <nadhiren.naidoo@gmail.com>
2022-11-07 23:02:57 +0200

    added functions

---
+++ pages/api/post/insertLocation.js
@@ -0,0 +1,36 @@
+const mysql = require('mysql');
+
+const pool = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'sql11.freesqldatabase.com',
+  user            : 'sql11525614',
+  password        : '9xKG7CAuX5',
+  database        : 'sql11525614'
+
+})
+
+
+export default function handler(req, res) {
+
+  pool.getConnection((err,connection) => {
+
+    if(err) throw err
+    console.log('connected as id')
+    const params = req.body
+
+    connection.query('INSERT INTO user_location set ?',params,(err,rows)=> {
+       connection.release()
+
+       if(!err)
+       {
+           res.send(`users with param ${params} has been added`)
+       }
+       else
+       {
+           console.log(err)
+       }
+    })
+})
+
+  //res.status(200).json({ name: 'John Doe' })
+}
