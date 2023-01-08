8ee1c47747b4318feb080639bb88503f7a64efe3
nadz1234 <nadhiren.naidoo@gmail.com>
2023-01-08 23:53:54 +0200

    Update [store].js

--- pages/api/get/countOrdersMonth/[store].js
+++ pages/api/get/countOrdersMonth/[store].js
@@ -20,7 +20,7 @@ export default function handler(req, res) {
     if(err) throw err
     console.log('connected as id')
 
-    connection.query('SELECT COUNT(*) as ordersToday FROM orders JOIN user_location ON orders.user_id = user_location.user_id AND user_location.store = ? WHERE DATE(orders.timeOfOrder) = MONTH(CURDATE())', [req.query.store], (err,rows)=> {
+    connection.query('SELECT COUNT(*) as ordersToday FROM orders JOIN user_location ON orders.user_id = user_location.user_id AND user_location.store = ? WHERE DATE(orders.timeOfOrder) = MONTH(CURRENT_DATE())', [req.query.store], (err,rows)=> {
        connection.release() 
 
        if(!err)
